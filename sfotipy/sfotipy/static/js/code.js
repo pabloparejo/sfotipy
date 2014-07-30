1c3f9e7f1b740fd3439085d992cfa9da213cf2ae
pabloparejo <parejo.p@gmail.com>
2014-07-30 16:05:42 +0200

    Handlebars added

--- sfotipy/sfotipy/static/js/code.js
+++ sfotipy/sfotipy/static/js/code.js
@@ -1,16 +1,75 @@
 var path = location.pathname.split('/')[1];
 
-console.log(path);
-
 function setActive(){
 	if (!path){
-		path = 'home';
+		path = 'home'
 	}
-	$('#' + path).addClass('active');
-
-	if (path == "checkout"){
-		$cart_btn.addClass('active')
-	}
+	$('#' + path).addClass('active')
 }
 
 setActive();
+
+
+Sfotipy = {}
+
+Sfotipy.Song = Backbone.Model.extend({})
+
+Sfotipy.SongsCollection = Backbone.Collection.extend({
+    model: Sfotipy.Song
+})
+
+Sfotipy.SongView = Backbone.View.extend({
+
+    events: {
+        'click .action.icon-add': 'add'
+    },
+
+    tagName: 'li',
+    className: 'item border-bottom',
+    template: Handlebars.compile($('#song-template').html()),
+
+    add: function (e){
+        alert(this.model.get('name'))
+        e.preventDefault()
+    },
+
+    initialize: function(){
+        this.listenTo(this.model, "change", this.render, this)
+    },
+
+    render: function () {
+        var html = this.template(this.model.toJSON())
+        console.log(this.el)
+        this.$el.prepend(html)
+    }
+})
+
+Sfotipy.Router = Backbone.Router.extend({
+    routes: {
+        "": "index",
+        "album/:name": "album",
+        "profile/:username": "profile"
+    },
+
+    index: function(){
+        console.log("index");
+    },
+
+    album: function(name) {
+        console.log("Album:" + name)
+    },
+
+    profile: function(username){
+        console.log("Username:" + username)
+    }
+
+})
+
+Sfotipy.app = new Sfotipy.Router()
+Backbone.history.start({pushState: true})
+
+window.Sfotipy = Sfotipy;
+
+
+
+
